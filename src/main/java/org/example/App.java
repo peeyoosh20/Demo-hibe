1a8df33f5932ba420576f204c7bd7eabb937a72c
peeyoosh20 <peeyooshmani@gmail.com>
2021-09-13 09:50:59 +0530

    added fetching

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,18 +19,23 @@ public class App
         alien.setColor("Brown");
         alien.setaName(new AlienName("Peeyoosh","Mani",
                 "Tiwari"));
-        alien.setaId(102);
-        alien.setColor("white");
-        alien.setaName(new AlienName("James","Pal",
+        Alien alien2=new Alien();
+        alien2.setaId(102);
+        alien2.setColor("white");
+        alien2.setaName(new AlienName("James","Pal",
                 "Anderson"));
         Configuration configuration=new Configuration().configure()
                 .addAnnotatedClass(Alien.class);
         SessionFactory sessionFactory=configuration.buildSessionFactory();
         Session session=sessionFactory.openSession();
         Transaction transaction=session.beginTransaction();
-       // alien=session.get(Alien.class,102);
         session.save(alien);
+        session.save(alien2);
+        alien=session.get(Alien.class,101);
+       alien2=session.get(Alien.class,102);
+
         transaction.commit();
         System.out.println(alien);
+        System.out.println(alien2);
     }
 }
